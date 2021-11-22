fb5f6d7fca3baed16be2f704e62a0c7c1ecc10bd
Peter Akaliro <pin4eva@hotmail.com>
2021-11-22 16:03:10 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import HeaderComp from "./components/HeaderComp";
+import HomePage from "./pages";
+import AboutPage from "./pages/about";
+
+const App = () => {
+	return (
+		<Router>
+			<div>
+				<HeaderComp />
+				<Switch>
+					<Route path="/" exact component={HomePage} />
+					<Route path="/about" component={AboutPage} />
+				</Switch>
+			</div>
+		</Router>
+	);
+};
+
+export default App;
---
+++ src/components/HeaderComp.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const HeaderComp = () => {
+	return (
+		<Header>
+			<nav>
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+				</ul>
+			</nav>
+		</Header>
+	);
+};
+
+export default HeaderComp;
+
+const Header = styled.header`
+	background-color: red;
+`;
