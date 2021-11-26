6dc6a834875fec3270dd79000485eba5795bfaa1
dike <dchinyere62@gmail.com>
2021-11-26 11:18:31 +0100

    added more utility classes

--- src/components/HeaderComp.jsx
+++ src/components/HeaderComp.jsx
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const HeaderComp = () => {
-	return (
-		<Header>
-			<nav>
-				<ul>
-					<li>
-						<Link to="/">Home</Link>
-					</li>
-					<li>
-						<Link to="/about">About</Link>
-					</li>
-				</ul>
-			</nav>
-		</Header>
-	);
+  return (
+    <Header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">AboutUs</Link>
+          </li>
+          <li>
+            <Link to="/about">ContactUS</Link>
+          </li>
+        </ul>
+      </nav>
+    </Header>
+  );
 };
 
 export default HeaderComp;
 
 const Header = styled.header`
-	/* background-color: red; */
+  /* background-color: red; */
 `;
